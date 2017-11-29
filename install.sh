62bbb255979d238b2851a845e514ec84a1d5ba93
Singh, Yuvraj <ts-yuvraj.singh@rakuten.com>
2017-11-29 13:26:58 -0500

    version 1

---
+++ install.sh
@@ -0,0 +1,12 @@
+export NAMESPACE=default && \
+export DOCKER_REPOSITORY=nanit && \
+export RABBITMQ_REPLICAS=5 && \
+export RABBITMQ_DEFAULT_USER=username && \
+export RABBITMQ_DEFAULT_PASS=password && \
+export RABBITMQ_ERLANG_COOKIE=secret && \
+export RABBITMQ_EXPOSE_MANAGEMENT=TRUE && \
+export RABBITMQ_MANAGEMENT_SERVICE_TYPE=LoadBalancer && \
+export RABBITMQ_HA_POLICY='{\"ha-mode\":\"all\"}' && \
+export RABBITMQ_LOG_LEVEL=info && \
+export SUDO="" && \
+make deploy
