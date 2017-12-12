48931408992c04c89bda7e965ab183f5e6592427
Yuvraj Singh <ts-yuvraj.singh@rakuten.com>
2017-12-12 21:09:18 +0000

    1.0.1

--- remove.sh
+++ remove.sh
@@ -3,4 +3,5 @@
 kubectl delete services rabbitmq 
 kubectl delete services rabbitmq-management 
 kubectl delete services rmq-cluster 
+kubectl delete --all pods
 kubectl delete statefulset rabbitmq 
